7b8195a46e649a6b9c21c62fb6e243f08a93610c
jakeggreen <80248380+jakeggreen@users.noreply.github.com>
2021-03-21 13:46:03 +0000

    Update Elementary 11.py

--- Elementary/Elementary 11.py
+++ Elementary/Elementary 11.py
@@ -5,23 +5,19 @@ Input: A string.
 Output: An iterable of strings."""
 
 def split_pairs(a):
-	x = list(a[::])
+	n = 2
 	if not len(a) % 2 == 0:
-		x.append('_')
-	ran = int(len(x)/2)
-	for i in range(0, ran ,2):
-		print(x[i] + x[i+1])
-
-print(split_pairs('abcd'))
+		a = a + '_'
+	return (a[i:i+n] for i in range(0, len(a), n))
 				
-# if __name__ == '__main__':
-# 	print("Example:")
-# 	print(list(split_pairs('abcd')))
+if __name__ == '__main__':
+	print("Example:")
+	print(list(split_pairs('abcd')))
 
-# 	# These "asserts" are used for self-checking and not for an auto-testing
-# 	assert list(split_pairs('abcd')) == ['ab', 'cd']
-# 	assert list(split_pairs('abc')) == ['ab', 'c_']
-# 	assert list(split_pairs('abcdf')) == ['ab', 'cd', 'f_']
-# 	assert list(split_pairs('a')) == ['a_']
-# 	assert list(split_pairs('')) == []
-# 	print("Coding complete? Click 'Check' to earn cool rewards!")
+	# These "asserts" are used for self-checking and not for an auto-testing
+	assert list(split_pairs('abcd')) == ['ab', 'cd']
+	assert list(split_pairs('abc')) == ['ab', 'c_']
+	assert list(split_pairs('abcdf')) == ['ab', 'cd', 'f_']
+	assert list(split_pairs('a')) == ['a_']
+	assert list(split_pairs('')) == []
+	print("Coding complete? Click 'Check' to earn cool rewards!")
