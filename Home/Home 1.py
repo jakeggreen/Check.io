4bb4f8b902852571bfc1f4b85731f5168b1d472d
jakeggreen <80248380+jakeggreen@users.noreply.github.com>
2021-03-22 21:21:54 +0000

    Create Home 1.py

---
+++ Home/Home 1.py
@@ -0,0 +1,8 @@
+"""In a given text you need to sum the numbers. Only separated numbers should be counted. If a number is part of a word it shouldn't be counted.
+
+The text consists from numbers, spaces and english letters
+
+Input: A string.
+
+Output: An int."""
+
