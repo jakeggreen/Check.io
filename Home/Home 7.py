21805ec331985bea7af4072eac2282f81e080e1a
jakeggreen <80248380+jakeggreen@users.noreply.github.com>
2021-03-25 19:33:41 +0000

    Update Home 7.py

--- Home/Home 7.py
+++ Home/Home 7.py
@@ -21,7 +21,6 @@ def backward_string_by_word(text: str) -> str:
 	if not len(text) == 0: #deals with empty strings
 		text = text.split(' ') #split on spaces making sure they're included
 		reverse = [word[::-1] for word in text] #use slice to reverse each word
-		reverse.append("x")
 		return ' '.join(reverse) #join words back together including spaces
 	return text
 
@@ -36,6 +35,3 @@ if __name__ == '__main__':
 	assert backward_string_by_word('hello   world') == 'olleh   dlrow'
 	assert backward_string_by_word('welcome to a game') == 'emoclew ot a emag'
 	print("Coding complete? Click 'Check' to earn cool rewards!")
-
-
-range(0,5)
