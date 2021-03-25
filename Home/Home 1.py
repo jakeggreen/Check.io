5a9505883cebb58e9358315eb2e5adcbb629f204
jakeggreen <80248380+jakeggreen@users.noreply.github.com>
2021-03-25 11:42:17 +0000

    New challenges

--- Home/Home 1.py
+++ Home/Home 1.py
@@ -6,3 +6,40 @@ Input: A string.
 
 Output: An int."""
 
+#method 1 checking each word for letters and removing if alpha.
+# def sum_numbers(text: str) -> int:
+#     text = text.split(' ')
+#     for word in text:
+#     	if word.isalpha():
+#     		text.remove(word)
+#     print(text)
+
+#method 2 - using regular expressions check is word contains numbers and or alpha characters
+# import re
+
+# def sum_numbers(text: str) -> int:
+# 	summation = list()
+# 	for word in text.split():
+# 		if not re.search(r'[^0-9\s]+',word):
+# 			summation.append(int(word))
+# 	return(sum(summation))
+
+#method 3 - condensed version without reg ex
+
+def sum_numbers(text: str) -> int:
+	return sum(int(x) for x in text.split() if x.isdigit())
+
+if __name__ == '__main__':
+	print("Example:")
+	print(sum_numbers('daa3 d 63'))
+
+	# These "asserts" are used for self-checking and not for an auto-testing
+	assert sum_numbers('hi') == 0
+	assert sum_numbers('who is 1st here') == 0
+	assert sum_numbers('my numbers is 2') == 2
+	assert sum_numbers('This picture is an oil on canvas '
+ 'painting by Danish artist Anna '
+ 'Petersen between 1845 and 1910 year') == 3755
+	assert sum_numbers('5 plus 6 is') == 11
+	assert sum_numbers('') == 0
+	print("Coding complete? Click 'Check' to earn cool rewards!")
---
+++ Home/Home 4.py
@@ -0,0 +1,34 @@
+"""You are given a string where you have to find its first word.
+
+When solving a task pay attention to the following points:
+
+There can be dots and commas in a string.
+A string can start with a letter or, for example, a dot or space.
+A word can contain an apostrophe and it's a part of a word.
+The whole text can be represented with one word and that's it.
+Input: A string.
+
+Output: A string."""
+
+#method 1 using one line and regex to findall words that have zero or more apostrophes within the word
+
+import re
+
+def first_word(text: str) -> str:
+	# print(re.sub(r'[^(\w)(\')]',' ',text).split())
+	# return re.sub(r'[^(\w)(\')]',' ',text).split()[0]
+	return re.findall(r'\w+\'*\w*',text)[0]
+
+if __name__ == '__main__':
+	print("Example:")
+	print(first_word("'Hello world"))
+
+	# These "asserts" are used for self-checking and not for an auto-testing
+	assert first_word("Hello world") == "Hello"
+	assert first_word(" a word ") == "a"
+	assert first_word("don't touch it") == "don't"
+	assert first_word("greetings, friends") == "greetings"
+	assert first_word("... and so on ...") == "and"
+	assert first_word("hi") == "hi"
+	assert first_word("Hello.World") == "Hello"
+	print("Coding complete? Click 'Check' to earn cool rewards!")
