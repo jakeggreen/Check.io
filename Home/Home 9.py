25ea29413c609a83e3d888e766da9f2a46a90577
jakeggreen <80248380+jakeggreen@users.noreply.github.com>
2021-03-26 13:12:20 +0000

    More challenges

--- Home/Home 9.py
+++ Home/Home 9.py
@@ -10,23 +10,25 @@ Input: Three arguments. All of them are strings. The second and third arguments 
 Output: A string."""
 
 def between_markers(text: str, begin: str, end: str) -> str:
-    """
-        returns substring between two given markers
-    """
-    # your code here
-    return ''
+	s = int(text.find(begin)+len(begin)) if begin in text else 0
+	e = int(text.find(end)) if end in text else len(text)
+	# testing statements
+	# print(int(text.find(begin)+len(begin)))
+	# print(int(text.find(end)))
+	# print(s,e)
+	return text[s:e]
 
 
 if __name__ == '__main__':
-    print('Example:')
-    print(between_markers('What is >apple<', '>', '<'))
+	print('Example:')
+	print(between_markers("Never send a human to do a machine's job.","Never","do"))
 
-    # These "asserts" are used for self-checking and not for testing
-    assert between_markers('What is >apple<', '>', '<') == "apple", "One sym"
-    assert between_markers("<head><title>My new site</title></head>",
-                           "<title>", "</title>") == "My new site", "HTML"
-    assert between_markers('No[/b] hi', '[b]', '[/b]') == 'No', 'No opened'
-    assert between_markers('No [b]hi', '[b]', '[/b]') == 'hi', 'No close'
-    assert between_markers('No hi', '[b]', '[/b]') == 'No hi', 'No markers at all'
-    assert between_markers('No <hi>', '>', '<') == '', 'Wrong direction'
-    print('Wow, you are doing pretty good. Time to check it!')
+	# These "asserts" are used for self-checking and not for testing
+	assert between_markers('What is >apple<', '>', '<') == "apple", "One sym"
+	assert between_markers("<head><title>My new site</title></head>",
+						   "<title>", "</title>") == "My new site", "HTML"
+	assert between_markers('No[/b] hi', '[b]', '[/b]') == 'No', 'No opened'
+	assert between_markers('No [b]hi', '[b]', '[/b]') == 'hi', 'No close'
+	assert between_markers('No hi', '[b]', '[/b]') == 'No hi', 'No markers at all'
+	assert between_markers('No <hi>', '>', '<') == '', 'Wrong direction'
+	print('Wow, you are doing pretty good. Time to check it!')
