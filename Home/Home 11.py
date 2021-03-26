fa27226ca6ba69dcdacbe04f1c691b80f9bc389a
jakeggreen <80248380+jakeggreen@users.noreply.github.com>
2021-03-26 23:40:40 +0000

    challenge

--- Home/Home 11.py
+++ Home/Home 11.py
@@ -12,29 +12,33 @@ Input: The text and the search words array.
 Output: The dictionary where the search words are the keys and values are the number of times when those words are occurring in a given text."""
 
 def popular_words(text: str, words: list) -> dict:
-    # your code here
-    return None
+	# d = {}
+	text = text.lower().split()
+	# for word in words:
+	# 	d[word] = text.count(word)
+	# return d
+	return {word: text.count(word) for word in words}
 
 
 if __name__ == '__main__':
-    print("Example:")
-    print(popular_words('''
+	print("Example:")
+	print(popular_words('''
 When I was One
 I had just begun
 When I was Two
 I was nearly new
 ''', ['i', 'was', 'three', 'near']))
 
-    # These "asserts" are used for self-checking and not for an auto-testing
-    assert popular_words('''
+	# These "asserts" are used for self-checking and not for an auto-testing
+	assert popular_words('''
 When I was One
 I had just begun
 When I was Two
 I was nearly new
 ''', ['i', 'was', 'three', 'near']) == {
-        'i': 4,
-        'was': 3,
-        'three': 0,
-        'near': 0
-    }
-    print("Coding complete? Click 'Check' to earn cool rewards!")
+		'i': 4,
+		'was': 3,
+		'three': 0,
+		'near': 0
+	}
+	print("Coding complete? Click 'Check' to earn cool rewards!")
