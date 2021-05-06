1646c4f26a4916fb06250533820bb6c8ce9db686
jakeggreen <80248380+jakeggreen@users.noreply.github.com>
2021-05-06 19:44:43 +0100

    Update Scientific Expedition 3 - The Hidden Word.py

--- Scientific Expedition/Scientific Expedition 3 - The Hidden Word.py
+++ Scientific Expedition/Scientific Expedition 3 - The Hidden Word.py
@@ -17,8 +17,9 @@ Output: The coordinates of the word."""
 import re 
 
 def checkio(text, word):
-	print(text)
-	print(re.sub(r',\s', r'\n',  text))
+	text = re.sub(r'[^\n\S\w]', '', text.lower())
+	number_of_rows = text.count('\n') + 1
+	word = list(word)	
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
