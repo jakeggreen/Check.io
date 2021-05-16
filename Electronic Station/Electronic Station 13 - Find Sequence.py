b883517f9fd2849f4497b394ee8cc6b5bbda417d
jakeggreen <80248380+jakeggreen@users.noreply.github.com>
2021-05-16 22:16:28 +0100

    Update Electronic Station 13 - Find Sequence.py

--- Electronic Station/Electronic Station 13 - Find Sequence.py
+++ Electronic Station/Electronic Station 13 - Find Sequence.py
@@ -8,36 +8,37 @@ Output: Whether or not a sequence exists as a boolean."""
 from typing import List
 
 def checkio(matrix: List[List[int]]) -> bool:
-    #replace this for solution
-    return True or False
+	for row in range(0,len(matrix)-1):
+		for column in range(0,len(matrix[0])-1):
+			print(row,column)
 
 if __name__ == '__main__':
-    #These "asserts" using only for self-checking and not necessary for auto-testing
-    assert checkio([
-        [1, 2, 1, 1],
-        [1, 1, 4, 1],
-        [1, 3, 1, 6],
-        [1, 7, 2, 5]
-    ]) == True
-    assert checkio([
-        [7, 1, 4, 1],
-        [1, 2, 5, 2],
-        [3, 4, 1, 3],
-        [1, 1, 8, 1]
-    ]) == False
-    assert checkio([
-        [2, 1, 1, 6, 1],
-        [1, 3, 2, 1, 1],
-        [4, 1, 1, 3, 1],
-        [5, 5, 5, 5, 5],
-        [1, 1, 3, 1, 1]
-    ]) == True
-    assert checkio([
-        [7, 1, 1, 8, 1, 1],
-        [1, 1, 7, 3, 1, 5],
-        [2, 3, 1, 2, 5, 1],
-        [1, 1, 1, 5, 1, 4],
-        [4, 6, 5, 1, 3, 1],
-        [1, 1, 9, 1, 2, 1]
-    ]) == True
-    print('All Done! Time to check!')
+	#These "asserts" using only for self-checking and not necessary for auto-testing
+	assert checkio([
+		[1, 2, 1, 1],
+		[1, 1, 4, 1],
+		[1, 3, 1, 6],
+		[1, 7, 2, 5]
+	]) == True
+	assert checkio([
+		[7, 1, 4, 1],
+		[1, 2, 5, 2],
+		[3, 4, 1, 3],
+		[1, 1, 8, 1]
+	]) == False
+	assert checkio([
+		[2, 1, 1, 6, 1],
+		[1, 3, 2, 1, 1],
+		[4, 1, 1, 3, 1],
+		[5, 5, 5, 5, 5],
+		[1, 1, 3, 1, 1]
+	]) == True
+	assert checkio([
+		[7, 1, 1, 8, 1, 1],
+		[1, 1, 7, 3, 1, 5],
+		[2, 3, 1, 2, 5, 1],
+		[1, 1, 1, 5, 1, 4],
+		[4, 6, 5, 1, 3, 1],
+		[1, 1, 9, 1, 2, 1]
+	]) == True
+	print('All Done! Time to check!')
