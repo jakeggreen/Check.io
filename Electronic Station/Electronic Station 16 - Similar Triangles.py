88d2b1ba905ba672fd274b3126e2dbda7c7d450f
jakeggreen <80248380+jakeggreen@users.noreply.github.com>
2021-05-30 12:15:28 +0100

    New exercises

--- Electronic Station/Electronic Station 16 - Similar Triangles.py
+++ Electronic Station/Electronic Station 16 - Similar Triangles.py
@@ -8,33 +8,40 @@ Coords = List[Tuple[int, int]]
 
 import math
 
+#use the math library acos to find the arccos of each angle
 def find_angles(a, b, c):
-	first_angle = (b**2 + c**2 - a**2) / 2*b*c
-	angle_a = math.cos(first_angle)
-	second_angle = (c**2 + a**2 - b**2) / 2*c*a
-	angle_b = math.cos(second_angle)
-	angle_c = 180 - (angle_a + angle_b)
-	return [angle_a, angle_b, angle_c]
+	first_angle = (a**2 + b**2 - c**2) / (2*a*b)
+	second_angle = (c**2 + b**2 - a**2) / (2*c*b)
+	third_angle = (a**2 + c**2 - b**2) / (2*c*a)
+	# print(first_angle)
+	try:
+		angle_a = math.acos(first_angle)
+		angle_b = math.acos(second_angle)
+		angle_c = math.acos(third_angle)
+		return [angle_a, angle_b, angle_c]
+	except:
+		return False
+	
 
 #find three lengths of sides of triangle, find the semi perimeter and then the area of triangle.
 
 def similar_triangles(coords_1: Coords, coords_2: Coords) -> bool:
-	#find distances between points, A-B, B-C, C-A for both triangles
+	#find length of sides between points, A-B, B-C, C-A for both triangles
 	coord_list = (coords_1, coords_2)
 	angles = []
 	for coord in coord_list:
-		a = coord[2][0] - coord[0][0]
-		b = coord[1][1] - coord[0][1]
-		c = coord[1][1] - coord[2][1]
+		a = abs(coord[2][0] - coord[0][0])
+		b = abs(coord[1][1] - coord[0][1])
+		c = abs(coord[1][1] - coord[2][1])
 		print(a,b,c)
 		angles.append(find_angles(a, b, c))
-	print(angles)
+		print(angles)
 	return True if angles[0] == angles[1] else False
 	
 	
 if __name__ == '__main__':
 	print("Example:")
-	print(similar_triangles([(0, 0), (1, 2), (2, 0)], [(3, 0), (4, 2), (5, 0)]))
+	print(similar_triangles([[0, 2], [1, 4], [5, 2]], [[0, 1], [1, 3], [5, 1]]))
 
 	# These "asserts" are used for self-checking and not for an auto-testing
 	assert similar_triangles([(0, 0), (1, 2), (2, 0)], [(3, 0), (4, 2), (5, 0)]) is True, 'basic'
@@ -43,4 +50,5 @@ if __name__ == '__main__':
 	assert similar_triangles([(0, 0), (1, 2), (2, 0)], [(2, 0), (4, 4), (6, 0)]) is True, 'scaling'
 	assert similar_triangles([(0, 0), (0, 3), (2, 0)], [(3, 0), (5, 3), (5, 0)]) is True, 'reflection'
 	assert similar_triangles([(1, 0), (1, 3), (2, 0)], [(3, 0), (5, 5), (5, 0)]) is False, 'different #2'
+	assert similar_triangles([[1, 3], [4, 2], [2, 1]], [[2, -2], [0, -3], [-1, -1]]) is True
 	print("Coding complete? Click 'Check' to earn cool rewards!")
---
+++ Incinerator/Incinerator 1 - The Warriors.py
@@ -0,0 +1,60 @@
+"""I'm sure that many of you have some experience with computer games. But have you ever wanted to change the game so that the characters or a game world would be more consistent with your idea of the perfect game? Probably, yes.
+In this mission (and in several subsequent ones, related to it) you’ll have a chance "to sit in the developer's chair" and create the logic of a simple game about battles.
+Let's start with the simple task - one-on-one duel. You need to create the class Warrior , the instances of which will have 2 parameters - health (equal to 50 points) and attack (equal to 5 points), and 1 property - is_alive, which can be True (if warrior's health is > 0) or False (in the other case). In addition you have to create the second unit type - Knight, which should be the subclass of the Warrior but have the increased attack - 7. Also you have to create a function fight() , which will initiate the duel between 2 warriors and define the strongest of them. The duel occurs according to the following principle:
+Every turn, the first warrior will hit the second and this second will lose his health in the same value as the attack of the first warrior. After that, if he is still alive, the second warrior will do the same to the first one.
+The fight ends with the death of one of them. If the first warrior is still alive (and thus the other one is not anymore), the function should return True , False otherwise."""
+
+class Warrior():
+	def __init__(self):
+		self.name = __name__
+		self.health = 50
+		self.attack = 5
+		self.is_alive = True
+
+	def make_attack(self, unit_1, unit_2):
+		print(f'Attacker hits defender for {unit_1.attack}.')
+		unit_2.health = unit_2.health - unit_1.attack
+		print(f'Defender has {unit_2.health} health remaining')
+		if unit_2.health <= 0:
+			unit_2.is_alive = False
+			return unit_2.is_alive
+		return unit_2.health
+
+	def turn(self, unit_1, unit_2):
+		unit_1.make_attack(unit_1, unit_2)
+		if unit_2.is_alive == True:
+			unit_2.make_attack(unit_2, unit_1)
+			unit_1.turn(unit_1, unit_2)
+		else:
+			return True
+		
+
+class Knight(Warrior):
+	def __init__(self):
+		super().__init__()
+		self.attack = 7
+
+def fight(unit_1, unit_2):
+
+	print(unit_1.name, unit_2.name)
+	unit_1.turn(unit_1, unit_2)
+
+if __name__ == '__main__':
+	#These "asserts" using only for self-checking and not necessary for auto-testing
+
+	chuck = Warrior()
+	bruce = Warrior()
+	carl = Knight()
+	dave = Warrior()
+	mark = Warrior()
+
+	assert fight(dave, carl) == False
+	assert fight(chuck, bruce) == True
+	assert chuck.is_alive == True
+	assert bruce.is_alive == False
+	assert carl.is_alive == True
+	assert dave.is_alive == False
+	assert fight(carl, mark) == False
+	assert carl.is_alive == False
+
+	print("Coding complete? Let's try tests!")
