4e53afdba01d5e2df0447ca4d34e7d6e58ad7182
jakeggreen <80248380+jakeggreen@users.noreply.github.com>
2021-05-30 12:37:56 +0100

    Update Incinerator 1 - The Warriors.py

--- Incinerator/Incinerator 1 - The Warriors.py
+++ Incinerator/Incinerator 1 - The Warriors.py
@@ -6,27 +6,33 @@ The fight ends with the death of one of them. If the first warrior is still aliv
 
 class Warrior():
 	def __init__(self):
-		self.name = __name__
 		self.health = 50
 		self.attack = 5
 		self.is_alive = True
 
-	def make_attack(self, unit_1, unit_2):
-		print(f'Attacker hits defender for {unit_1.attack}.')
-		unit_2.health = unit_2.health - unit_1.attack
-		print(f'Defender has {unit_2.health} health remaining')
-		if unit_2.health <= 0:
-			unit_2.is_alive = False
-			return unit_2.is_alive
-		return unit_2.health
+
+	def make_attack(self, attacker, defender):
+		# print(f'Attacker hits defender for {attacker.attack}.')
+		defender.health = defender.health - attacker.attack
+		# print(f'Defender has {defender.health} health remaining')
+		if defender.health <= 0:
+			defender.is_alive = False
+			return defender.is_alive
+		return defender.health
 
 	def turn(self, unit_1, unit_2):
+		# print(f'unit 1 attacks unit 2')
 		unit_1.make_attack(unit_1, unit_2)
 		if unit_2.is_alive == True:
+			# print(f'unit 2 attacks unit 1')
 			unit_2.make_attack(unit_2, unit_1)
-			unit_1.turn(unit_1, unit_2)
+			if unit_1.is_alive == True:
+				unit_1.turn(unit_1, unit_2)
+			else:
+				unit_1.is_alive = False
+				return unit_1.is_alive
 		else:
-			return True
+			return unit_2.is_alive
 		
 
 class Knight(Warrior):
@@ -36,8 +42,8 @@ class Knight(Warrior):
 
 def fight(unit_1, unit_2):
 
-	print(unit_1.name, unit_2.name)
-	unit_1.turn(unit_1, unit_2)
+	return unit_2.is_alive if unit_1.turn(unit_1, unit_2) else unit_1.is_alive
+
 
 if __name__ == '__main__':
 	#These "asserts" using only for self-checking and not necessary for auto-testing
