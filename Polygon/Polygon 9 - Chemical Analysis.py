fa06f5348e512eca9063c1a1172f53563b2be19a
jakeggreen <80248380+jakeggreen@users.noreply.github.com>
2021-06-14 18:21:14 +0100

    Update Polygon 9 - Chemical Analysis.py

--- Polygon/Polygon 9 - Chemical Analysis.py
+++ Polygon/Polygon 9 - Chemical Analysis.py
@@ -7,25 +7,40 @@ Output: List of the chemical elements, sorted in the alphabetical order."""
 
 import re
 
-def rec(formula, multiplier):
+def outside_rec(formula, multiplier, elements = None):
 
-	elements = {}
+	if elements is None:
+		elements = {}
 
 	#need to deal with anything outside of the brackets
 
 	outside_brackets = re.findall(r'([A-Z][a-z]*)(\d*)[\(\[]\S+[\)\]]\d*', formula)
+
+	print(outside_brackets)
 	
 	if outside_brackets:
 		for each in outside_brackets:
 			z = 1 if each[1] == '' else int(each[1])
 			elements.update({each[0]: m + (z * multiplier)}) if (m := elements.get(each[0])) else elements.update({each[0]: z})
 
-	brackets = re.findall(r'[\(\[](\S+)[\)\]](\d*)', formula)
+def brackets_rec(formula, multiplier, elements = None):
+
+	if elements is None:
+		elements = {}
+
+	outside_rec(formula, 1, elements)
+
+	brackets = re.findall(r'[A-Z]*[a-z]*[\(\[](\S+)[\)\]](\d*)', formula)
+
+	print(brackets, "set of brackets")
 
 	if brackets:
+		print(brackets[0][0])
+		outside_rec(brackets[0][0], multiplier, elements)
 		for item in brackets:
 			j = 1 if item[1] == '' else int(item[1])
-			for y in rec(item[0], j * multiplier):
+			for y in brackets_rec(item[0], j, elements):
+				print(y, "y", multiplier)
 				elements.update({y: s + (j * multiplier)}) if (s := elements.get(y)) else elements.update({y: j})
 	else:
 		arr = re.findall(r'([A-Z][a-z]*)(\d*)', formula)
@@ -33,13 +48,13 @@ def rec(formula, multiplier):
 			i = 1 if x[1] == '' else int(x[1])
 			elements.update({x[0]: s + (i * multiplier)}) if (s := elements.get(x[0])) else elements.update({x[0]: i})
 	
-	print(elements)
+	print(elements, "here")
 	
 	return elements
 
 def atoms(formula, limit):
 
-	elements = rec(formula, 1)
+	elements = brackets_rec(formula, 1)
 
 	ordered_list = sorted([item for item in elements if elements.get(item) >= limit], reverse = False)
 
@@ -48,12 +63,12 @@ def atoms(formula, limit):
 	return ordered_list
 
 if __name__ == '__main__':
-	print("Example:")
-	print(atoms('C2H5OH', 2))
+	# print("Example:")
+	# print(atoms('C2H5OH', 2))
 
 	#These "asserts" using only for self-checking and not necessary for auto-testing
-	assert atoms('C2H5OH2', 2) == ['C', 'H']
-	assert atoms('H2O', 2) == ['H']
-	assert atoms('Mg(OH)2', 1) == ['H', 'Mg', 'O']
+	# assert atoms('C2H5OH2', 2) == ['C', 'H']
+	# assert atoms('H2O', 2) == ['H']
+	# assert atoms('Mg(OH)2', 1) == ['H', 'Mg', 'O']
 	assert atoms('K4[ON(SO3)2]2', 4) == ['K', 'O', 'S']
 	print("Coding complete? Click 'Check' to earn cool rewards!")
