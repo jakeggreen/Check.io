7dd4976491044b712160face81ddab181676a50b
jakeggreen <80248380+jakeggreen@users.noreply.github.com>
2022-02-15 15:30:37 +0000

    More exercises

--- Scientific Expedition/Scientific Expedition 14 - Conversion from CamelCase.py
+++ Scientific Expedition/Scientific Expedition 14 - Conversion from CamelCase.py
@@ -4,18 +4,22 @@ Input: A function name as a CamelCase string.
 
 Output: The same string, but in under_score."""
 
+import re
+
 def from_camel_case(name: str) -> str:
-    #replace this for solution
-    return name
+	for x in name[1:]:
+		if x == x.upper():
+			name = re.sub(x, f"_{x.lower()}", name)
+	return name.lower()
 
 if __name__ == '__main__':
-    print("Example:")
-    print(from_camel_case("Name"))
+	# print("Example:")
+	# print(from_camel_case("Name"))
 
-    #These "asserts" using only for self-checking and not necessary for auto-testing
-    assert from_camel_case("MyFunctionName") == "my_function_name"
-    assert from_camel_case("IPhone") == "i_phone"
-    assert from_camel_case("ThisFunctionIsEmpty") == "this_function_is_empty"
-    assert from_camel_case("Name") == "name"
-    print("Coding complete? Click 'Check' to earn cool rewards!")
+	#These "asserts" using only for self-checking and not necessary for auto-testing
+	assert from_camel_case("MyFunctionName") == "my_function_name"
+	assert from_camel_case("IPhone") == "i_phone"
+	assert from_camel_case("ThisFunctionIsEmpty") == "this_function_is_empty"
+	assert from_camel_case("Name") == "name"
+	print("Coding complete? Click 'Check' to earn cool rewards!")
 
